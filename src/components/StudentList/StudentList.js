06ddef209a4af57689a0eeb45994904e2c857c5b
mingyu <mgahn0706@snu.ac.kr>
2021-09-16 09:04:35 +0900

    Added empty case
    학생이 비었을 때 문구 출력

--- src/components/StudentList/StudentList.js
+++ src/components/StudentList/StudentList.js
@@ -5,28 +5,53 @@ import StudentItem from "./StudentItem/StudentItem";
 const StudentList = ({studentList, showDetail, selectedStudentID}) => {
 
 
+    if(studentList.length===0){
+        return (
+            <div className={"listSection"}>
 
-    return (
-        <div className={"listSection"}>
-            <table className="studentListSection">
-                <thead>
+                <table className="studentListSection">
+                    <thead>
                     <tr className="listHeader">
                         <th className="nameHeader">이름</th>
                         <th className="gradeHeader">학년</th>
                         <th className="blankHeader"></th>
                     </tr>
-                </thead>
-                <tbody>
-                        {studentList.map((item) => (
-                            (selectedStudentID===item.id ? <StudentItem key={item.id} item={item} showDetail={showDetail} isSelected={true} />
-                                : <StudentItem key={item.id} item={item} showDetail={showDetail} isSelected={false} />)
-                            /*만약 새로 추가된 학생이라면 initial state 를 selected 되게 하도록 설정한다.*/
-                        ))}
+                    </thead>
 
-                </tbody>
-            </table>
-        </div>
-    );
+                </table>
+
+                <div className="emptyListSection">
+                학교에 학생이 없어요 :(
+
+                </div>
+            </div>
+        );
+    }
+    else {
+        return (
+            <div className={"listSection"}>
+
+                <table className="studentListSection">
+                    <thead>
+                    <tr className="listHeader">
+                        <th className="nameHeader">이름</th>
+                        <th className="gradeHeader">학년</th>
+                        <th className="blankHeader"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {studentList.map((item) => (
+                        (selectedStudentID === item.id ?
+                            <StudentItem key={item.id} item={item} showDetail={showDetail} isSelected={true}/>
+                            : <StudentItem key={item.id} item={item} showDetail={showDetail} isSelected={false}/>)
+                        /*만약 새로 추가된 학생이라면 initial state 를 selected 되게 하도록 설정한다.*/
+                    ))}
+
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 export default StudentList;
