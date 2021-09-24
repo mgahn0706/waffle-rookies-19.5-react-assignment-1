06c33f37f300458b2e5321c574aac705f2a60013
mingyu <mgahn0706@snu.ac.kr>
2021-09-25 01:30:09 +0900

    update README

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className="Header" >
             <a href="https://wafflestudio.com" target="_blank" rel="noreferrer">
-                <img src="https://wafflestudio.com/_next/image?url=%2Fimages%2Ficon_intro.svg&w=256&q=75" alt="waffleLogo" className={"waffleLogo"}></img>
+                <img src="https://wafflestudio.com/_next/image?url=%2Fimages%2Ficon_intro.svg&w=256&q=75" alt="waffleLogo" className={"waffleLogo"}/>
             </a>
 
             <span className="title">와플고등학교 명단 관리 프로그램 </span>
