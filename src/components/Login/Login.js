e05aee3f3d4ca4793ebb1a24dbfd9c20ac48cef1
mingyu <mgahn0706@snu.ac.kr>
2021-10-07 04:13:22 +0900

    로그인 리디렉션

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -21,7 +21,7 @@ const Login = () => {
                 localStorage.setItem('token',response.data.access_token)
                 setLogin(true);
                 request.defaults.headers.common['Authorization'] = `Bearer ${response.data.access_token}`
-                
+                history.replace('/students')
 
 
             })
